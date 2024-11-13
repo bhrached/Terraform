708c51e407b694b001b1eb36469e8670e355ba17
Rached BEN HASSINE <bhrached@gmail.com>
2024-11-13 15:14:04 +0100

    deplaacement fichier vagrantfile 2

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -1,8 +1,8 @@
 resource "vagrant_vm" "my_vagrant_vm" {
-   vagrantfile_dir = "../vagrant_ansible/"
+   vagrantfile_dir = "${var.vagrantfile_dir}"
   env = {
     # force terraform to re-run vagrant if the Vagrantfile changes
-    VAGRANTFILE_HASH = md5(file("../vagrant_ansible/Vagrantfile")),
+    VAGRANTFILE_HASH = md5(file("${var.vagrantfile_dir}Vagrantfile")),
   }
   get_ports = true
   # see schema for additional options
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "vagrantfile_dir" {
+  description = "The directory containing the Vagrantfile"
+  type        = string
+  default     = "../vagrant_ansible/"  # Valeur par défaut
+}
