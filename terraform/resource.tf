810d5296ecbd26bc4f3348ebbe4dc7684c42f19a
Rached BEN HASSINE <bhrached@gmail.com>
2024-11-13 14:21:48 +0100

    depmacement fichier vagrantfile

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -1,7 +1,8 @@
 resource "vagrant_vm" "my_vagrant_vm" {
+   vagrantfile_dir = "../vagrant_ansible/"
   env = {
     # force terraform to re-run vagrant if the Vagrantfile changes
-    VAGRANTFILE_HASH = md5(file("./Vagrantfile")),
+    VAGRANTFILE_HASH = md5(file("../vagrant_ansible/Vagrantfile")),
   }
   get_ports = true
   # see schema for additional options
